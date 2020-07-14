1a0b5eb88652667a9de0cc2cb2856351bb4ac0c9
entgelme <entgelme@de.ibm.com>
2020-07-14 16:26:13 +0200

    initial

---
+++ src/main/java/com/aedemo/integratingspringandjsf/IntegratingSpringAndJsfApplication.java
@@ -0,0 +1,13 @@
+package com.aedemo.integratingspringandjsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegratingSpringAndJsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegratingSpringAndJsfApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/aedemo/integratingspringandjsf/service/DayOfWeekResolverService.java
@@ -0,0 +1,6 @@
+package com.aedemo.integratingspringandjsf.service;
+
+public interface DayOfWeekResolverService
+{
+  String determineDayOfWeek(int year, int month, int dayOfMonth);
+}
